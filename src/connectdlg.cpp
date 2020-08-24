099cf5a740cddda23123541b63e2adfc5fec1ac0
chigkim <chigkim@users.noreply.github.com>
2020-08-23 20:41:46 -0400

    Fixed crash when rapidly changing genre.

--- src/connectdlg.cpp
+++ src/connectdlg.cpp
@@ -812,6 +812,7 @@ void CConnectDlg::SetPingTimeAndNumClientsResult ( const CHostAddress& InetAddr,
             pCurListViewItem->
                 setText ( 1, QString ( "%1 ms" ).arg ( iMinPingTime, 4, 10, QLatin1Char ( ' ' ) ) );
         }
+        dynamic_cast<QPushButton*>(lvwServers->itemWidget(pCurListViewItem,1))->setText(pCurListViewItem->text(1));
 
         // update number of clients text
         if ( pCurListViewItem->text ( 5 ).toInt() == 0 )
@@ -827,6 +828,7 @@ void CConnectDlg::SetPingTimeAndNumClientsResult ( const CHostAddress& InetAddr,
         {
             pCurListViewItem->setText ( 2, QString().setNum ( iNumClients ) + "/" + pCurListViewItem->text ( 5 ) );
         }
+        dynamic_cast<QPushButton*>(lvwServers->itemWidget(pCurListViewItem, 2))->setText(pCurListViewItem->text(2));
 
         // check if the number of child list items matches the number of
         // connected clients, if not then request the client names
@@ -874,8 +876,6 @@ void CConnectDlg::SetPingTimeAndNumClientsResult ( const CHostAddress& InetAddr,
     // we may have changed the Hidden state for some items, if a filter was active, we now
     // have to update it to void lines appear which do not satisfy the filter criteria
     UpdateListFilter();
-    dynamic_cast<QPushButton*>(lvwServers->itemWidget(pCurListViewItem,1))->setText(pCurListViewItem->text(1));
-    dynamic_cast<QPushButton*>(lvwServers->itemWidget(pCurListViewItem, 2))->setText(pCurListViewItem->text(2));
 }
 
 QTreeWidgetItem* CConnectDlg::FindListViewItem ( const CHostAddress& InetAddr )
